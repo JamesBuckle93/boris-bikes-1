5eb0bb85623169a5dd6862cea2d5b09e6b797130
Jonah Massam <Jonah.Massam10@yahoo.co.uk>
2022-01-19 15:32:07 +0000

    Q_13_Done

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,32 @@
 require_relative 'Bike'
 
 class DockingStation
+  attr_reader :bike_storage
+
+  def initialize
+    @bike_storage = []
+    for i in (0..4)
+      @bike_storage.push(Bike.new)
+    end
+  end
+
   def release_bike
-    Bike.new
+    if @bike_storage.length == 0
+      fail "No Bikes!"
+    else
+      @bike_storage.pop
+    end
+  end
+
+  def store_bike(bike)
+    if bike.is_a? Bike
+      if @bike_storage.length < 5
+        bike_storage.push(bike)
+      else
+        fail "Full!"
+      end
+    else
+      fail "Not A Bike!"
+    end
   end
 end
--- spec/dockingstaion_spec.rb
+++ spec/dockingstaion_spec.rb
@@ -10,4 +10,32 @@ describe DockingStation do
   bike = subject.release_bike 
   expect(bike).to be_working 
   end
+
+  it "stores a bike" do
+    docking_station = DockingStation.new
+    new_bike = Bike.new
+    docking_station.release_bike
+    docking_station.store_bike(new_bike)
+    expect(docking_station.bike_storage).to include new_bike
+  end
+
+  it "no bikes error" do
+    docking_station = DockingStation.new
+    docking_station.release_bike
+    docking_station.release_bike
+    docking_station.release_bike
+    docking_station.release_bike
+    docking_station.release_bike
+    expect {docking_station.release_bike}.to raise_error "No Bikes!"
+  end
+
+  it "can't store bike" do
+    docking_station = DockingStation.new
+    expect {docking_station.store_bike("test")}.to raise_error "Not A Bike!"
+  end
+
+  it "storage full" do
+    docking_station = DockingStation.new
+    expect {docking_station.store_bike(Bike.new)}.to raise_error "Full!"
+  end
 end
